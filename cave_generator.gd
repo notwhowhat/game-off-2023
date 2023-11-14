8b06375e0da00b979d22a965e5659b99a532fd25
whowhat <jakobthefoodmonster@gmail.com>
2023-11-14 16:44:24 +0100

    basically fully working generation, except for some spaces are gray

--- cave_generator.gd
+++ cave_generator.gd
@@ -38,9 +38,11 @@ func generate(pos=Vector2(0, 0)):
 	
 	var map_pos = tile_map.local_to_map(pos)
 	
-	for x in range(-self.map_width / 2, self.map_width / 2):
-		for y in range(-self.map_height / 2, self.map_height / 2):
-			var noise_pos = self.noise.get_noise_2d(x + pos.x, y)
+	#for x in range(-self.map_width / 2, self.map_width / 2):
+	#	for y in range(-self.map_height / 2, self.map_height / 2):
+	for x in range(self.map_width):
+		for y in range(self.map_height):
+			var noise_pos = self.noise.get_noise_2d(map_pos.x + x - map_width / 2, map_pos.y + y - map_height / 2)
 			if noise_pos < self.noise_threshold:
 				#self._set_tile(x + x - pos.x, y + y - pos.y, noise_pos)
 				self._set_tile(map_pos.x + x - map_width / 2, map_pos.y + y - map_height / 2, noise_pos)
@@ -48,7 +50,7 @@ func generate(pos=Vector2(0, 0)):
 
 func _set_tile(x, y, noise_pos):
 	var pos = Vector2i(x, y)
-	self.tile_map.set_cell(0, pos, 0, Vector2(0, randi() % 2))
+	self.tile_map.set_cell(0, pos, 0, Vector2(0, int(noise_pos * 10) % 2))
 	
 
 
