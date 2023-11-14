4f22136f2813b003d2e5568f93ca6229d0773fb8
whowhat <jakobthefoodmonster@gmail.com>
2023-11-14 18:57:45 +0100

    optimized the generation from some horrible system i found in a tutorial might be a bit unreadable but it should do

--- cave_generator.gd
+++ cave_generator.gd
@@ -21,6 +21,8 @@ extends Node
 
 var tile_map: TileMap
 var noise = FastNoiseLite.new()
+var prev_gen_tile_coords = {}
+
 
 func _ready():
 	self.tile_map = get_parent() as TileMap
@@ -39,17 +41,23 @@ func generate(pos=Vector2(0, 0)):
 	
 	var map_pos = tile_map.local_to_map(pos)
 	
-	#for x in range(-self.map_width / 2, self.map_width / 2):
-	#	for y in range(-self.map_height / 2, self.map_height / 2):
 	for x in range(self.map_width):
 		for y in range(self.map_height):
-			var noise_pos = self.noise.get_noise_2d(map_pos.x + x - map_width / 2, map_pos.y + y - map_height / 2)
+			var noise_pos
+			var tile_pos = Vector2(map_pos.x + x - map_width / 2, map_pos.y + y - map_height / 2)
+			if tile_pos in prev_gen_tile_coords:
+				noise_pos = prev_gen_tile_coords.get(tile_pos)
+			else: 
+				noise_pos = self.noise.get_noise_2d(tile_pos.x, tile_pos.y)
+				prev_gen_tile_coords[tile_pos] = noise_pos
+			#var noise_pos = self.noise.get_noise_2d(tile_pos.x, tile_pos.y)
 			#if noise_pos < self.noise_threshold:
 				#self._set_tile(x + x - pos.x, y + y - pos.y, noise_pos)
-			self._set_tile(map_pos.x + x - map_width / 2, map_pos.y + y - map_height / 2, noise_pos)
+			self._set_tile(tile_pos.x, tile_pos.y, noise_pos)
 				#print(pos.x)
 			#else:
 			#	self._set_tile(map_pos.x + x - map_width / 2, map_pos.y + y - map_height / 2, noise_pos)
+			
 				
 				
 func _set_tile(x, y, noise_pos):
@@ -61,5 +69,5 @@ func _set_tile(x, y, noise_pos):
 
 
 func _on_player_redraw():
-	clear()
+	#clear()
 	generate(player.position)
